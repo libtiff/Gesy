ab44b7a4ade49e31f60ce68e991f5ad2ec97dc67
LibTiff <bboyjcm@hotmail.com>
2020-02-25 16:03:56 +0200

    Connection Check Implementation and Toast

--- app/src/main/java/libtiff/gesy/MainActivity.java
+++ app/src/main/java/libtiff/gesy/MainActivity.java
@@ -1,7 +1,10 @@
 package libtiff.gesy;
 
 import android.app.Activity;
+import android.content.Context;
+import android.net.ConnectivityManager;
 import android.net.http.SslError;
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
 import android.webkit.CookieManager;
 import android.webkit.CookieSyncManager;
@@ -29,10 +32,13 @@ public class MainActivity extends Activity
 
         CookieSyncManager.createInstance(this);
         CookieManager.getInstance().setAcceptCookie(true);
-        MobileAds.initialize(this, "ca-app-pub-9082725429338291~9699329161");
+        MobileAds.initialize(this, "ca-app-pub-9082725429338291~4697500512");
         AdRequest adRequest = new AdRequest.Builder().build();
         mAdView.loadAd(adRequest);
         final Activity activity = this;
+
+        WifiManager wifi = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+        ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
 
         webview1.setWebViewClient(new WebViewClient()
         {
@@ -49,23 +55,25 @@ public class MainActivity extends Activity
             }
         });
 
-        webview1.getSettings().setSupportZoom(true);
-        webview1.getSettings().setBuiltInZoomControls(true);
-        webview1.getSettings().setDisplayZoomControls(false);
-        webview1.getSettings().setDomStorageEnabled(true);
-        webview1.getSettings().setJavaScriptEnabled(true);
-        webview1.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
-        webview1.getSettings().setUseWideViewPort(true);
-        webview1.getSettings().setLoadWithOverviewMode(true);
-        webview1.getSettings().setDefaultTextEncodingName("utf-8");
-        webview1 .loadUrl("https://portal.gesy.org.cy/dashboard");
+        if (wifi.isWifiEnabled() || connectivityManager.getActiveNetworkInfo()!= null){
+            //wifi/data is enabled
+            webview1.getSettings().setSupportZoom(true);
+            webview1.getSettings().setBuiltInZoomControls(true);
+            webview1.getSettings().setDisplayZoomControls(false);
+            webview1.getSettings().setDomStorageEnabled(true);
+            webview1.getSettings().setJavaScriptEnabled(true);
+            webview1.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
+            webview1.getSettings().setUseWideViewPort(true);
+            webview1.getSettings().setLoadWithOverviewMode(true);
+            webview1.getSettings().setDefaultTextEncodingName("utf-8");
+            webview1 .loadUrl("https://portal.gesy.org.cy/dashboard");
+        }
+        else
+        {
+            Toast.makeText(getApplicationContext(),"No internet connection , please enable your wifi or data connection. Ενεργοποιήστε το ίντερνετ ή τα δεδομένα σας για να μπορέσει να τρέξει η εφαρμογή.",Toast.LENGTH_LONG).show();
+            finish();
+        }
     }
-
-    public void checkpermissions()
-    {
-
-    }
-
     @Override
     public void onBackPressed()
     {
